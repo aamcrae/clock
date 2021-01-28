e840f6140323fa04eb4a5648d34778b9b5d63e20
Andrew McRae <amcrae@google.com>
2021-01-28 20:23:12 +1100

    Added PWM driver

---
+++ example/pwm.go
@@ -0,0 +1,58 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Program to demonstrate how to access the s/w PWM library
+
+package main
+
+import (
+	"flag"
+	"log"
+	"math"
+	"time"
+
+	"github.com/aamcrae/clock/io"
+)
+
+var pwmPin = flag.Int("gpio", 18, "GPIO pin for PWM example")
+
+func main() {
+	flag.Parse()
+	pin, err := io.OutputPin(*pwmPin)
+	if err != nil {
+		log.Fatalf("Pin %d: %v", *pwmPin, err)
+	}
+	defer pin.Close()
+	pwm := io.NewPWM(pin)
+	defer pwm.Close()
+	for i := 0; i < 10; i++ {
+		for v := 0; v < 90; v++ {
+			set(pwm, v)
+		}
+		for v := 89; v >= 0; v-- {
+			set(pwm, v)
+		}
+	}
+}
+
+func set(pwm *io.PWM, v int) {
+	period := time.Millisecond * 2
+	r := float64(v) * math.Pi / 180
+	d := int(math.Sin(r) * 100.0)
+	err := pwm.Set(period, d)
+	if err != nil {
+		log.Fatalf("Set: period %s, duty %d: %v", period.String(), d, err)
+	}
+	time.Sleep(time.Millisecond * 50)
+}
---
+++ io/pwm.go
@@ -0,0 +1,95 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package io
+
+import (
+	"fmt"
+	"time"
+)
+
+type pwmMsg struct {
+	period time.Duration
+	duty int	// Duty cycle as percentage
+	stop  chan bool
+}
+
+type PWM struct {
+	pin Setter
+	c               chan pwmMsg
+}
+
+// NewPWM creates a new s/w PWM controller.
+func NewPWM(pin Setter) (* PWM) {
+	p := new(PWM)
+	p.pin = pin
+	p.c = make(chan pwmMsg, 1)
+	go p.handler()
+	return p
+}
+
+// Close closes the PWM controller
+func (p *PWM) Close() {
+	sc := make(chan bool)
+	p.c <- pwmMsg{0, 0, sc}
+	<-sc
+	close(sc)
+	close(p.c)
+}
+
+// Set sets the PWM parameters. The changes take
+// place at the end of the current period.
+func (p *PWM) Set(period time.Duration, duty int) error {
+	if duty < 0 || duty > 100 {
+		return fmt.Errorf("%d: invalid duty cycle percentage")
+	}
+	p.c <- pwmMsg{period, duty, nil}
+	return nil
+}
+
+// goroutine handler
+// Listens on message channel, and runs the PWM.
+func (p *PWM) handler() {
+	var on, off time.Duration
+	off = time.Millisecond * 5
+	current := 0
+	p.pin.Set(0)
+	for {
+		if on != 0 {
+			if current != 1 {
+				p.pin.Set(1)
+				current = 1
+			}
+			time.Sleep(on)
+		}
+		if off != 0 {
+			if current != 0 {
+				p.pin.Set(0)
+				current = 0
+			}
+			time.Sleep(off)
+		}
+		// Check for new parameters after each cycle.
+		select {
+		case m := <-p.c:
+			if m.stop != nil {
+				m.stop <- true
+				return
+			}
+			on = m.period * time.Duration(m.duty) / 100
+			off = m.period - on
+		default:
+		}
+	}
+}
